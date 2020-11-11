f630b0664a973c8180f3b83826e396eb6a41bcc1
Nemo <nemoleerink@gmail.com>
2020-11-11 20:52:17 +0100

    -

---
+++ Voorbeelden/Demo-cs/HalloWin3/HalloWin3.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows.Forms;
+using System.Drawing;
+
+class HalloForm : Form
+{
+    public HalloForm()
+    {
+        this.Text = "Hallo";
+        this.BackColor = Color.Yellow;
+        this.Size = new Size(300, 100);
+        this.Paint += this.tekenScherm;
+    }
+
+    void tekenScherm(object obj, PaintEventArgs pea)
+    {
+        pea.Graphics.DrawString( "Hallo iedereen!"
+                               , new Font("Tahoma", 30)
+                               , Brushes.Blue
+                               , 10, 10
+                               );
+    }
+}
+
+class HalloWin3
+{
+    static void Main()
+    {
+        HalloForm scherm;
+        scherm = new HalloForm();
+        Application.Run(scherm);
+    }
+}
---
+++ Voorbeelden/Demo-cs/KleurKlasse/Voorbeeld.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace KleurKlasse
+{
+    class Voorbeeld
+    {
+        static void Main()
+        {
+            Kleur wit, paars, oranje, lichtgrijs, donkergrijs;
+
+            wit = new Kleur();
+            paars = new Kleur(255, 0, 255);
+            oranje = new Kleur(255, 128, 0);
+            lichtgrijs = new Kleur(180);
+            donkergrijs = new Kleur(60);
+
+            byte x = oranje.Grijswaarde();
+            Kleur oranjeInZwartwit = new Kleur(x);
+
+            oranje.MaakDonkerder();
+            string s = oranje.ToString();
+
+            Kleur donkerPaars = paars.DonkerdereVersie();
+            Kleur donkerGeel = Kleur.Geel.DonkerdereVersie();
+
+            Console.WriteLine($"DonkerOranje: {oranje}");
+            Console.WriteLine($"DonkerPaars: {donkerPaars}");
+            Console.WriteLine($"DonkerGeel: {donkerGeel}");
+            Console.ReadLine();
+        }
+    }
+}
