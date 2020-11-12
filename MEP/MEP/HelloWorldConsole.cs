85ff5e839d6e6a055b86c5a55bacc7ffbafb6d41
Nemo <nemoleerink@gmail.com>
2020-11-12 13:06:00 +0100

    -

---
+++ MEP/MEP/HelloWorldConsole.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace HelloWorldConsole
+{
+    class HelloWorldConsole
+    {
+        static void Main(string[] args)
+        {
+            string naam;
+            Console.WriteLine("Wat is je naam?");
+            naam = Console.ReadLine();
+            Console.WriteLine("Hallo, " + naam + "!");
+            Console.WriteLine("Je naam heeft " + naam.Length + " letters.");
+
+            // Now is property van DateTime. DateTime is een static class. ToString is een methode die op een DateTime object kan worden uitgevoerd.
+            String date = DateTime.Now.ToString("dd/MM/yyy");
+            Console.WriteLine(date);
+
+            Console.WriteLine("Wat is je geboortejaar?");
+            string geboortejaar = Console.ReadLine();
+
+            int geboortejaarint = int.Parse(geboortejaar);
+
+            int geschatteleeftijd;
+            geschatteleeftijd = DateTime.Now.Year - geboortejaarint;
+
+            // Console.WriteLine(DateTime.Now.Year.GetType());
+            Console.WriteLine("Geschatte leeftijd is " + geschatteleeftijd);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ MEP/WindowsForm/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            this.BackColor = Color.Yellow;
+            this.Paint += Teken;
+        }
+
+        public void Teken(object o, PaintEventArgs pea) 
+        {
+            pea.Graphics.DrawLine(Pens.Red, 20, 20, 40, 60);
+            pea.Graphics.DrawEllipse(Pens.Black, 100, 30, 30, 100);
+            pea.Graphics.DrawLine(new Pen(Color.Red, 5), 20, 50, 50, 100);
+        
+        }
+
+    }
+}
